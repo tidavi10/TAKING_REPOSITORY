626245258fa76b41b68f3e39f25054a156c85adf
Rafael Santana <rgs1990419@gmail.com>
2021-02-11 16:40:58 -0300

    Styled-components

--- sistema_de_chamados/src/pages/Login/styles.js
+++ sistema_de_chamados/src/pages/Login/styles.js
@@ -1,9 +1,38 @@
 import styled from 'styled-components';
 
 export const Container= styled.div`
-    background: blue;
-    width: 200px;
-    height: 200px;
-
+    /* max-width: 800px; */
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 80px 100px;
 `;
 //transform: translate(110 %, 6 %);
+
+export const Title = styled.div`
+    font-size: 20px;
+    margin-bottom: 24px;
+`;
+
+export const Form = styled.div`
+    width: 400px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    background: #ff8717;
+    border-radius: 10px;
+    padding: 32px 24px;
+`;
+
+export const Input = styled.input`
+    width: 100%;
+    height: 24px;
+`;
+
+export const Button = styled.button`
+    width: 100%;
+    height: 24px;
+`;
