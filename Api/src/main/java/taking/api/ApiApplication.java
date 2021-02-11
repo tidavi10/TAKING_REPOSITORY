515fb6b825b147c3ec41d07342b068b0b9604c63
JhowDarks <yaneduardo123@gmail.com>
2021-02-11 12:23:28 -0300

    Inserção Spring Boot

--- Api/src/main/java/taking/api/ApiApplication.java
+++ Api/src/main/java/taking/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.taking.Api;
+package taking.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
--- Api/src/test/java/taking/api/ApiApplicationTests.java
+++ Api/src/test/java/taking/api/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.taking.Api;
+package taking.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
