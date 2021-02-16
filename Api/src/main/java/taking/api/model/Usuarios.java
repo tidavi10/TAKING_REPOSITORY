dbf8e7931edd382de7fe6c91f6ca995988eeb22e
raphaelmartineztaking <raphael.martinez@taking.com.br>
2021-02-16 11:28:56 -0300

    Usuarios

---
+++ Api/src/main/java/taking/api/model/Usuarios.java
@@ -0,0 +1,126 @@
+package taking.api.model;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+public class Usuarios implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "username")
+	private String username;
+	
+	private String email;
+	
+	@Column(name = "senha")
+	private String senha;
+	
+	private String nome;
+	
+	private String endereco;
+	
+	private String cep;
+	
+	private String telefone;
+	
+	private String cpf;
+	
+	private String rg;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	
+	public void JwtRequest()
+	{
+	}
+
+	public void JwtRequest(String username, String senha) {
+		this.setUsername(username);
+		this.setSenha(senha);
+	}
+	
+	
+}
