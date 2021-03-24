597f198c52476b69fba6696a655036b47af22112
AlexGimenes2001 <80415019+AlexGimenes2001@users.noreply.github.com>
2021-03-24 15:14:12 -0300

    Login Social

--- sistema_de_chamados/src/components/PrivRoute/PrivateRoutes.js
+++ sistema_de_chamados/src/components/PrivRoute/PrivateRoutes.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Redirect, Route } from 'react-router'
 
 const PrivateRoute = props => {
-    const islogged = !!localStorage.getItem('@chamadosTaking:usuario')
+    const islogged = !!localStorage.getItem('@chamadosTaking:usuario') ||  !!localStorage.getItem('@chamadosTaking:usuarioSocial')
     return islogged ? <Route {...props} /> : <Redirect to="/" />
 }
 
