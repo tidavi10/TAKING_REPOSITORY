53250c1f5c1dcc77e78132302c6e215b411cbe34
AlexGimenes2001 <80415019+AlexGimenes2001@users.noreply.github.com>
2021-03-29 18:02:35 -0300

    teste

---
+++ sistema_de_chamados/src/components/PrivRoute/Redirect.js
@@ -0,0 +1,10 @@
+/*import React from 'react'
+
+const redirect = props => {
+    console.log(location)
+    if(location.protocol!=='http:'){
+        const htppUrl = 'http://' + location.href.split('//')[1]
+    }
+}
+
+export default redirect*/
